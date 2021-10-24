5f78a2e9dad36a70e7057e32cd6cf55f50f2e3be
Ahtisham Alam <ahtisham_101@outlook.com>
2021-10-24 13:12:09 +0500

    optionals in typescript

---
+++ optionals.js
@@ -0,0 +1,19 @@
+function printIngredient(quantity, ingredient, extra) {
+    console.log(quantity + " " + ingredient + " " + (extra ? " " + extra : ""));
+}
+printIngredient("1C", "Flour");
+printIngredient("1C", "Sugar", "something more");
+function getEmail(user) {
+    if (user.info) {
+        return user.info.email;
+    }
+    return "";
+}
+function getEmailEasy(user) {
+    var _a, _b;
+    return (_b = (_a = user === null || user === void 0 ? void 0 : user.info) === null || _a === void 0 ? void 0 : _a.email) !== null && _b !== void 0 ? _b : "";
+}
+function addWithCallback(x, y, callback) {
+    console.log([x, y]);
+    callback === null || callback === void 0 ? void 0 : callback();
+}
